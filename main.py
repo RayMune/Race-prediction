592252e2cf31823d4cd94c17a03d68699ede1078
RayMune <128319362+RayMune@users.noreply.github.com>
2023-06-01 06:25:46 +0000

    Decided to use random forests

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
-# Read the Excel sheet into Python.
-df = pd.read_excel("F1.xlsx")
+# Step 1: Load the data from the Excel sheet into a Pandas DataFrame
+df = pd.read_excel('F1 table.xlsx')
 
-# Create a new column for the predicted finishing position.
-df["predicted_finishing_position"] = np.nan
+# Step 2: Prepare the data for training the model
+X = df.iloc[:, 1:-1]  # Input features (finishing time for previous races)
+y = df.iloc[:, -1]    # Target variable (finishing time for the final race)
 
-# Loop over each driver.
-for driver in df["driver"].unique():
+# Step 3: Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Get the data for the current driver.
-    driver_data = df[df["driver"] == driver]
+# Step 4: Choose a regression model and train it on the training data
+model = RandomForestRegressor()  # You can replace this with any other regression model
+model.fit(X_train, y_train)
 
-    # Fit a linear regression model to the data.
-    model = LinearRegression()
-    model.fit(driver_data[["p1", "p2", "p3", "quali"]], driver_data["final"])
+# Step 5: Predict the finishing time for the final race using the trained model
+final_race_predictions = model.predict(X_test)
 
-    # Predict the finishing position for the current driver.
-    predicted_finishing_position = model.predict([[driver, driver, driver, driver]])[0]
+# Step 6: Evaluate the performance of the model using mean squared error (MSE)
+#mse = mean_squared_error(y_test, final_race_predictions)
 
-    # Set the predicted finishing position in the DataFrame.
-    df.loc[df["driver"] == driver, "predicted_finishing_position"] = predicted_finishing_position
+# Optional: Retrain the model on the entire dataset and make predictions for the final race
+#model.fit(X, y)
+#X_final_race = ...  # Input features for the final race
+#final_race_predictions = model.predict(X_final_race)
 
-# Print the predicted finishing positions.
-print(df["predicted_finishing_position"])
   
